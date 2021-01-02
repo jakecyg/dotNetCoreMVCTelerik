24a3b9e45a7aaa54769a3d7def95534491bc11b6
Jake Choi <JakeCYG@gmail.com>
2021-01-01 23:57:42 -0600

    added PiesController and views

---
+++ dotNetCoreMVCTelerikGrid/Controllers/PiesController.cs
@@ -0,0 +1,29 @@
+﻿using dotNetCoreMVCTelerikGrid.Models;
+using dotNetCoreMVCTelerikGrid.Services.Abstraction;
+using dotNetCoreMVCTelerikGrid.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreMVCTelerikGrid.Controllers
+{
+    public class PiesController : Controller
+    {
+        private readonly IPieRepo _pieRepo;
+        public PiesController(IPieRepo pieRepo) => _pieRepo = pieRepo;
+        public ViewResult PieList (string category)
+        {
+            IEnumerable<Pie> pies;
+            pies = category == null ? _pieRepo.GetAllPies : _pieRepo.GetAllPies.Where(x => x.Category.CategoryName == category);
+            var vm = new PieVM
+            {
+                Pies = pies
+            };
+            return View(vm);
+        }
+    }
+
+}
---
+++ dotNetCoreMVCTelerikGrid/ViewModels/PieVM.cs
@@ -0,0 +1,15 @@
+﻿using dotNetCoreMVCTelerikGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreMVCTelerikGrid.ViewModels
+{
+    public class PieVM
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public Pie Pie { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
