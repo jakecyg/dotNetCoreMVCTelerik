8c0feddc6a7434a25d823b28191cbaf7d1c9bb2c
Jake Choi <JakeCYG@gmail.com>
2021-01-01 19:57:49 -0600

    added pie and category models

---
+++ dotNetCoreMVCTelerikGrid/Models/Pie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreMVCTelerikGrid.Models
+{
+    public class Pie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDesc { get; set; }
+        public string LongDesc { get; set; }
+        public string AllergyInfo { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public string Notes { get; set; }
+        public int CategoryId { get; set; }
+        // Nav prop
+        public Category Category { get; set; }
+    }
+}
